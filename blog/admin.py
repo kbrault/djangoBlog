eec68bb831afc095f89a57a9001ed76236d9c9d4
Kevin <kbrault@protonmail.com>
2024-11-01 19:02:16 +0100

    persistant volume for db

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 
 class PostAdmin(admin.ModelAdmin):
-    exclude = ('slug',)  # Exclude slug from admin interface
+    exclude = ('slug',)
 
 admin.site.register(Post, PostAdmin)
+admin.site.unregister(Group)
+admin.site.unregister(User)
